5670e90322cf0523dfdf5dd5189ea8bd53f29f65
EkaterinaRozhkova <69673336+EkaterinaRozhkova@users.noreply.github.com>
2026-01-19 13:43:04 +0300

    fix

--- src/extensions/ResizableImageExtension.ts
+++ src/extensions/ResizableImageExtension.ts
@@ -92,46 +92,15 @@ export const CustomResizableImage = ImageResize.extend({
     // Добавляем ширину к wrapper div если есть caption
     const finalWrapperStyle = wrapperStyle;
     if (caption) {
-      // Берем ширину из исходных атрибутов ноды
-      // const width = node.attrs.width || imageAttrs.width;
-      // // Обязательно добавляем px если это число или строка-число
-      // let widthValue: string | undefined;
-      // let numericWidth = 0;
+      const width = node.attrs.width || imageAttrs.width;
+      console.log(width, 'width')
 
-      // if (width) {
-      //   if (typeof width === 'number') {
-      //     numericWidth = width;
-      //     widthValue = `${width}px`;
-      //   } else if (/^\d+$/.test(width)) {
-      //     // Если это строка содержащая только цифры, добавляем px
-      //     numericWidth = parseInt(width, 10);
-      //     widthValue = `${width}px`;
-      //   } else {
-      //     widthValue = width;
-      //   }
-      // }
 
-      // Вычисляем padding из wrapperStyle
-      // let paddingAdjustment = 0;
-      // const paddingLeftMatch = wrapperStyle.match(/padding-left:\s*(\d+)px/i);
-      // const paddingRightMatch = wrapperStyle.match(/padding-right:\s*(\d+)px/i);
-      // if (paddingLeftMatch) {
-      //   paddingAdjustment += parseInt(paddingLeftMatch[1], 10);
-      // }
-      // if (paddingRightMatch) {
-      //   paddingAdjustment += parseInt(paddingRightMatch[1], 10);
-      // }
 
-      // // Ширина для span с учетом padding
-      // const descriptionWidth = numericWidth > 0 && paddingAdjustment > 0
-      //   ? `${numericWidth - paddingAdjustment}px`
-      //   : widthValue;
-      //
-      // console.log('ResizableImage renderHTML:', { width, widthValue, descriptionWidth, paddingAdjustment, wrapperStyle, caption });
 
       content.push(['span', {
         class: 'image-description',
-        style: `display: block; font-size: 11px; line-height: 18px; font-weight: 400; color: #7D7D7D; text-align: center; maxWidth: 100%; box-sizing: border-box; overflow-wrap: break-word;`
+        style: `display: block; font-size: 11px; line-height: 18px; font-weight: 400; color: #7D7D7D; text-align: center; maxWidth: ${imageAttrs!.width + 'px'}; box-sizing: border-box; overflow-wrap: break-word;`
       }, caption]);
 
       // Обязательно добавляем ширину к wrapper div
